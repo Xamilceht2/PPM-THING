4f0161529af3835d0f1dee19822a6706feddddc3
Xamilceht2 <ethan.cunanan.889@my.aquinas.me>
2025-04-10 15:00:18 +0000

    asf

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ class PPMimage:
         row = []
         rcounter = 0
         row_val = int(self.dimensions.split()[1]) * 3
+        carry = []
         for line in file:
-            temp = line.split()
+            temp = carry
+            carry = []
+            temp += line.split()
             rcounter += len(temp)
-            print(temp, rcounter)
+            #print(temp, rcounter)
             if rcounter >= row_val:
                 if rcounter > row_val:
-                    for excess in range(rcounter - row_val):
-                        carry = temp[-excess]
-                        temp.pop(-1)
+                    remaining = rcounter - row_val
+                    for excess in range(remaining):
+                        carry.append(temp[-excess])
+                    del temp[-remaining:]
+                self.values.append(row)
+                row = []
+                
+                rcounter = 0
             for color in temp:
                 pcounter += 1
                 pixel.append(color)
                 if pcounter == 3:
-                    print(pixel)
+                    #print(pixel)
                     row.append(pixel)
                     pcounter = 0
                     pixel = []
-            print(row)
-            break
+            
+            
+        
                 
     def __init__(self, input, output):
         self.values = []
@@ -36,6 +45,10 @@ class PPMimage:
             self.dimensions = file.readline()
             self.maxval = file.readline()
             self.make_list(file)
+            self.values.pop(0)
+        with open(output) as outfile:
+            pass
     def print(self):
-        print(self.magic_num, self.dimensions)
-test = PPMimage("squares.ppm", "out.ppm")
+        print(self.magic_num, self.dimensions, self.values)
+test = PPMimage("test.ppm", "out.ppm")
+test.print()
