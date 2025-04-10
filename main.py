95c6cc2724b4039e8b3f0308286f3f4e14592c7c
Xamilceht2 <ethan.cunanan.889@my.aquinas.me>
2025-04-10 13:40:44 +0000

    df

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 class PPMimage:
     def make_list(self, file:str):
-        counter = -1
-        temp = file.split()
-        row = int(self.dimensions.split()[1])
-        for val in range(row):
-            pass
-        print(temp)
+        pcounter = 0
+        pixel = []
+        row = []
+        rcounter = 0
+        row_val = int(self.dimensions.split()[1]) * 3
+        for line in file:
+            temp = line.split()
+            rcounter += len(temp)
+            print(temp, rcounter)
+            if rcounter >= row_val:
+                if rcounter > row_val:
+                    for excess in range(rcounter - row_val):
+                        carry = temp[-excess]
+                        temp.pop(-1)
+            for color in temp:
+                pcounter += 1
+                pixel.append(color)
+                if pcounter == 3:
+                    print(pixel)
+                    row.append(pixel)
+                    pcounter = 0
+                    pixel = []
+            print(row)
+            break
+                
     def __init__(self, input, output):
         self.values = []
         self.magic_num = ''
         self.dimensions = ''
+        self.maxval = ''
 
         with open(input) as file:
             self.magic_num = file.readline()
             self.dimensions = file.readline()
+            self.maxval = file.readline()
             self.make_list(file)
     def print(self):
         print(self.magic_num, self.dimensions)
